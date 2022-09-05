1816996f5ff6c0ddb1117a1bc85d1c59690630a9
davay <fond@fondfrukt.ru>
2022-09-06 02:36:46 +0300

    deploy

---
+++ assets/_uid_.b106f36d.js
@@ -0,0 +1,2 @@
+import{b as e,D as a,o as r}from"./use.05207705.js";const s={__name:"[uid]",props:{uid:{type:String,default:""}},setup(t){return(n,o)=>(r(),e("p",null,"USER "+a(t.uid),1))}};export{s as default};
+//# sourceMappingURL=_uid_.b106f36d.js.map
